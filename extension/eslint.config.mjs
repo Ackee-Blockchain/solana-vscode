0f88b3842937b1192d94450d0d430c47b7489530
Max Kupchenko <maxkup19@gmail.com>
2025-05-14 15:09:27 +0200

    ad dlints

--- extension/eslint.config.mjs
+++ extension/eslint.config.mjs
@@ -1,11 +1,13 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import unusedImports from "eslint-plugin-unused-imports";
 
 export default [{
     files: ["**/*.ts"],
 }, {
     plugins: {
         "@typescript-eslint": typescriptEslint,
+        "unused-imports": unusedImports,
     },
 
     languageOptions: {
@@ -19,6 +21,18 @@ export default [{
             selector: "import",
             format: ["camelCase", "PascalCase"],
         }],
+
+        "@typescript-eslint/no-unused-vars": "off",
+        "unused-imports/no-unused-imports": "error",
+        "unused-imports/no-unused-vars": [
+            "warn",
+            {
+                "vars": "all",
+                "varsIgnorePattern": "^_",
+                "args": "after-used",
+                "argsIgnorePattern": "^_",
+            },
+        ],
 
         curly: "warn",
         eqeqeq: "warn",

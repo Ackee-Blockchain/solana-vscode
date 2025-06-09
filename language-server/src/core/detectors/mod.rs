6d3abc61b74ba5dd4feb46f3f9f071badfa05985
Maksym Kupchenko <94657478+maxkup19@users.noreply.github.com>
2025-06-09 20:10:27 +0200

    🪄 Sysvar account used detector (#6)
    * 🔥 Add sysvar account detector
    
    * ✅ add tests
    
    * add better comment
    
    * use anchor patterns

--- language-server/src/core/detectors/mod.rs
+++ language-server/src/core/detectors/mod.rs
@@ -2,8 +2,10 @@ pub mod detector;
 pub mod detector_config;
 pub mod manual_lamports_zeroing;
 pub mod missing_signer;
+pub mod sysvar_account_detector;
 pub mod unsafe_math;
 
 pub use manual_lamports_zeroing::*;
 pub use missing_signer::*;
+pub use sysvar_account_detector::*;
 pub use unsafe_math::*;

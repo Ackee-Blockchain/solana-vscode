ad152e901d8fa03bc630a7c809edbddfbebb32ad
hyckomat <hycko.matej@gmail.com>
2025-07-31 13:07:35 +0200

    Add rustc dependencies

--- language-server/tests/clippy_detector_tests.rs
+++ language-server/tests/clippy_detector_tests.rs
@@ -1,6 +1,6 @@
 // Integration tests for clippy detectors
 // This file allows cargo test to discover and run all clippy detector tests
-
+#![feature(rustc_private)]
 mod clippy_detectors {
     mod unsafe_math_detector_test;
 }
--- language-server/tests/clippy_detectors/unsafe_math_detector_test.rs
+++ language-server/tests/clippy_detectors/unsafe_math_detector_test.rs
@@ -1,13 +1,27 @@
+extern crate rustc_data_structures;
+extern crate rustc_errors;
+extern crate rustc_hir;
+extern crate rustc_lint;
+extern crate rustc_middle;
+extern crate rustc_session;
+extern crate rustc_span;
+
+use rustc_data_structures::fx::{FxHashMap, FxHashSet};
+use rustc_errors::Applicability;
+use rustc_hir::{Item, ItemKind, def_id::DefId};
+use rustc_lint::{LateContext, LateLintPass, LintPass, LintStore};
+use rustc_middle::ty::{self, Upcast};
+use rustc_session::{Session, declare_lint, impl_lint_pass};
+use rustc_span::{ExpnKind, MacroKind, Symbol, sym};
+
+use clippy_utils::{is_from_proc_macro, is_in_test_function, is_integer_literal};
 use language_server::core::detectors::{
     clippy_detectors::ClippyUncheckedArithmeticDetector,
     detector::{ClippyAnalysisContext, ClippyDetector, Detector},
 };
 use std::path::PathBuf;
 use tower_lsp::lsp_types::DiagnosticSeverity;
-
-use clippy_utils::{is_from_proc_macro, is_in_test_function, is_integer_literal};
-use rustc_hir as hir;
-use rustc_lint::{LateContext, LateLintPass};
+// use rustc_lint::{LateContext, LateLintPass};
 
 fn create_test_context(source_code: &str) -> ClippyAnalysisContext {
     todo!()
@@ -17,8 +31,18 @@ fn create_test_context(source_code: &str) -> ClippyAnalysisContext {
 #[derive(Copy, Clone)]
 struct ExampleLateLintPass;
 
+impl LintPass for ExampleLateLintPass {
+    fn name(&self) -> &'static str {
+        "example_late_lint_pass"
+    }
+
+    fn get_lints(&self) -> Vec<&'static rustc_lint::Lint> {
+        vec![]
+    }
+}
+
 impl<'tcx> LateLintPass<'tcx> for ExampleLateLintPass {
-    fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &'tcx hir::Expr<'tcx>) {
+    fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &'tcx rustc_hir::Expr<'tcx>) {
         // Use rustc_hir and clippy_utils
         if is_in_test_function(cx.tcx, expr.hir_id) {
             // Test logic
@@ -27,7 +51,7 @@ impl<'tcx> LateLintPass<'tcx> for ExampleLateLintPass {
 }
 
 // Example function using other clippy_utils methods
-fn example_clippy_utils<'tcx>(cx: &LateContext<'tcx>, expr: &'tcx hir::Expr<'tcx>) {
+fn example_clippy_utils<'tcx>(cx: &LateContext<'tcx>, expr: &'tcx rustc_hir::Expr<'tcx>) {
     let _ = is_integer_literal(expr, 0);
     let _ = is_from_proc_macro(cx, expr);
     // Add more clippy_utils calls as needed to test dependencies
@@ -40,6 +64,7 @@ mod tests {
     #[test]
     fn test_clippy_dependencies() {
         // This test ensures the code compiles with the dependencies
+
         assert!(true);
     }
 }
